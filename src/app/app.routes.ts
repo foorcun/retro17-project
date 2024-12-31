1d604d2bb9b6949276ccc06d8be6c0006edf540f
Furkan KAMACI <furkan.kamaci@amadeus.com>
2024-12-31 18:11:06 +0300

    grid example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home2Component } from './pages/home2/home2.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
     { path: "" , component: HomePageComponent },
+    // { path: "" , component: Home2Component },
 ];
